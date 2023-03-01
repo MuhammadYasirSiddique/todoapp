8e5a922d8f77afcd0d8c1ded6da1025376129f73
MuhammadYasir <imyasir79@yahoo.com>
2023-03-02 00:30:13 +0500

    rename page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import TodoList from "./todo-list";
 
 
-export default function Page(): JSX.Element{
+export default function Page(){ 
     return ( 
         <div  className="mx-auto p-1 border-2 border-gray-400  shadow-md shadow-gray-400 flex flex-wrapjustify-center lg:justify-start ">
             <div className="w-full md:w-1/2 p-4">
