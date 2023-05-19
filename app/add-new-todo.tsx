54ccca9e513bd4b1e8696a70572a894cfe73e73a
MuhammadYasir <imyasir79@yahoo.com>
2023-05-19 23:13:16 +0500

    Add Button type in Add new Todo Component

--- app/add-new-todo.tsx
+++ app/add-new-todo.tsx
@@ -1,48 +1,51 @@
-    "use client";
-    import { useRouter } from "next/navigation";
-    // import { stringify } from "querystring";
-    import { useState } from "react";
+"use client";
+import { useRouter } from "next/navigation";
+// import { stringify } from "querystring";
+import { useState } from "react";
 
-    async function addTodo(name: string, refresh: { (): void; (): void; }){
-        await fetch('/api/todo/add', {
-            method: "POST",
-            body: JSON.stringify({name}),
-        });
-        refresh();
+async function addTodo(name: string, refresh: { (): void; (): void }) {
+  await fetch("/api/todo/add", {
+    method: "POST",
+    body: JSON.stringify({ name }),
+  });
+  refresh();
+}
+
+export default function AddNewTodo() {
+  const router = useRouter();
+  let [name, setName] = useState("");
+
+  const handleAddTodo = async () => {
+    if (!name || name.trim() === "") {
+      alert("Todo cannot be empty.");
+      return;
     }
+    await addTodo(name, router.refresh);
+    setName("");
+  };
 
-    export default function AddNewTodo() {
-        const router = useRouter();
-        let [name, setName]= useState("");
+  return (
+    <div className="">
+      <div>
+        <label>
+          <input
+            type="text"
+            className="font-normal border border-slate-300 rounded-full py-1 pl-4 pr-4 shadow-md focus:outline-blue-500        placeholder:italic placeholder:text-slate-400 "
+            placeholder="Enter your task to do..."
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
 
-        const handleAddTodo = async () => {
-            if (!name || name.trim() === '') {
-                alert('Todo cannot be empty.');
-                return;
-            }
-            await addTodo(name, router.refresh);
-            setName("");
-        }
-
-        return(
-            <div className="">
-                <div>
-                    <label>
-                    <input type="text" className="font-normal border border-slate-300 rounded-full py-1 pl-4 pr-4 shadow-md focus:outline-blue-500        placeholder:italic placeholder:text-slate-400 " placeholder= "Enter your task to do..."
-                        onChange={(e) => setName(e.target.value)} 
-                        value ={name}  
-                    />
-                    </label>
-                
-                    <button 
-                        className=" mx-2 bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded-full shadow-lg mb-4 shadow-md shadow-gray-400"
-                        onClick={handleAddTodo}
-                    >
-                        Add Task
-                    </button>
-                    <br/>
-                </div>
-            </div>
-        );
-    }
-    
+        <button
+          type="button"
+          className=" mx-2 bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded-full shadow-lg mb-4 shadow-md shadow-gray-400"
+          onClick={handleAddTodo}
+        >
+          Add Task
+        </button>
+        <br />
+      </div>
+    </div>
+  );
+}
